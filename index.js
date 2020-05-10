8880b46b58fe07f6e7216a817c30ef17ee0b60c5
NumanWD <sevas85@gmail.com>
2020-05-10 12:27:03 +0100

    WIP

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const exec = require('@actions/exec');
+const github = require('@actions/github');
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+    const context = await github.context;
+    const pull_number = context.payload.issue.number;
+    const owner = context.payload.repository.full_name.split('/')[0];
+    const repo = context.payload.repository.name;
 
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+    
+    console.log(`pull_number ${pull_number}`)
+    console.log(`owner ${owner}`)
+    console.log(`repo ${repo}`)
 
-    core.setOutput('time', new Date().toTimeString());
+    core.debug(`owner ${owner}`);
   } 
   catch (error) {
     core.setFailed(error.message);
