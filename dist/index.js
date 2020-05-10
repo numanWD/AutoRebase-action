3a896dc1a8b8cef6ac40ba25ca3332dd25e241e5
NumanWD <sevas85@gmail.com>
2020-05-10 13:11:20 +0100

    WIP

--- dist/index.js
+++ dist/index.js
@@ -498,17 +498,18 @@ const github = __webpack_require__(469);
 async function run() {
   try { 
     const context = await github.context;
-    console.log(`pull_number ${console.log(JSON.stringify(context.payload))}`);
+    console.log(`info ${console.log(JSON.stringify(context))}`);
     // const pull_number = context.payload.issue.number;
     const owner = context.payload.repository.full_name.split('/')[0];
     const repo = context.payload.repository.name;
-
-    
-    // console.log(`pull_number ${pull_number}`)
-    console.log(`owner ${owner}`)
     console.log(`repo ${repo}`)
+    const pull_request = context.payload.pull_request;
 
-    core.debug(`owner ${owner}`);
+    if (! pull_request) {
+      core.setFailed(`this is not a pr`);
+    }
+    // console.log(`pull_number ${pull_number}`)
+    console.log(`Rebabled  ${context.payload.pull_request.rebaseable}`)
   } 
   catch (error) {
     core.setFailed(error.message);
